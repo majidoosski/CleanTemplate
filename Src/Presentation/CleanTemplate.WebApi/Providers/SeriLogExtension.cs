9db9fd162b89e881a364fb714cd89f28039746fa
majidoosski <majidakbaribeyzakhi@gmail.com>
2025-12-10 20:50:45 +0330

    fix: output template

--- Src/Presentation/CleanTemplate.WebApi/Providers/SeriLogExtension.cs
+++ Src/Presentation/CleanTemplate.WebApi/Providers/SeriLogExtension.cs
@@ -30,12 +30,15 @@ public class LoggerSetting : ILoggerSettings
          .MinimumLevel.Override("AspNetCore.Routing", LogEventLevel.Warning)
          .MinimumLevel.Override("Swagger", LogEventLevel.Warning)
          .WriteTo
-         .Console(Serilog.Events.LogEventLevel.Information, "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {NewLine}{Exception}", theme: AnsiConsoleTheme.Sixteen)
+         .Console(Serilog.Events.LogEventLevel.Information, "[{Timestamp:HH:mm:ss} {Level:u3}][Machine:{MachineName}][ThreadId:{ThreadId}][ThreadName:{ThreadName}] {Message:lj}{NewLine}{Exception}", theme: AnsiConsoleTheme.Sixteen)
          .Enrich.FromLogContext()
          .Enrich.WithThreadId()
          .Enrich.WithThreadName()
          .Enrich.WithMachineName()
-         .WriteTo.File("./Logs/log-.txt", LogEventLevel.Error, "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {NewLine}{Exception}" ,rollingInterval:RollingInterval.Day)
+         .WriteTo.File("./Logs/log-.txt", LogEventLevel.Error, "[{Timestamp:HH:mm:ss} {Level:u3}][Machine:{MachineName}][ThreadId:{ThreadId}][ThreadName:{ThreadName}] {Message:lj}{NewLine}{Exception}", rollingInterval:RollingInterval.Day)
          .Filter.ByExcluding("RequestPath like '/swagger%'");
     }
 }
+
+
+
