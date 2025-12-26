efa6818f676262663e41be7d054d229c7a9b7d02
majidoosski <majidakbaribeyzakhi@gmail.com>
2025-12-26 19:04:54 +0330

    fix: change behaviour of product controller

--- Src/Presentation/CleanTemplate.WebApi/Controllers/ProductController.cs
+++ Src/Presentation/CleanTemplate.WebApi/Controllers/ProductController.cs
@@ -3,7 +3,9 @@ using CleanTemplate.Application.Common.AppSettings;
 using CleanTemplate.Application.Contracts.ApplicationContracts;
 using CleanTemplate.Application.Contracts.Repositories;
 using CleanTemplate.Application.DTOs.Product;
+using CleanTemplate.Application.Services;
 using CleanTemplate.Application.Views.Product;
+using CleanTemplate.Application.Wrappers;
 using CleanTemplate.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,25 +16,34 @@ namespace CleanTemplate.WebApi.Controllers
     [Route("api/[controller]/[action]")]
     public class ProductController : ControllerBase
     {
-        private readonly IProductRepository _productRepository ;
+        private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
         private readonly ILogger<ProductController> _logger;
         private readonly IUnitOfWorkRepository _unitOfWorkRepository;
 
         public ProductController(IMapper mapper, IProductRepository productRepository, IUnitOfWorkRepository unitOfWorkRepository, ILogger<ProductController> logger)
         {
-            _mapper =mapper ;
+            _mapper = mapper;
             _productRepository = productRepository;
             _unitOfWorkRepository = unitOfWorkRepository;
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get Product
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Authorize]
-        public async Task<ActionResult<List<ProductView>>> Get(CancellationToken cancellationToken)
+        public async Task<ActionResult<ApplicationResponse<List<ProductView>>>> Get()
         {
-            var result = await _productRepository.GetAll();
-            return Ok(_mapper.Map<List<ProductView>>(result));
+            //var service = new GetRequestService<Product, long, ProductView>(_productRepository, _mapper);
+            var result= await _productRepository.GetAll();
+            var resultView=_mapper.Map<List<ProductView>>(result);
+            var response = new ApplicationResponse<List<ProductView>>(resultView);
+
+
+            return Ok(response);
         }
 
         //[Authorize(policy: "Manager")]
